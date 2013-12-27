3b640e39315d720b88e031b84f70eff7fa7deb68
qubecad <android@qubecad.co.uk>
2013-12-27 12:37:51 +0000

    QR code working and start on a server client (untested)

--- CoffeeHack/src/com/qubecad/coffeehack/customerloyality/MainActivity.java
+++ CoffeeHack/src/com/qubecad/coffeehack/customerloyality/MainActivity.java
@@ -1,17 +1,74 @@
 package com.qubecad.coffeehack.customerloyality;
 
-import android.os.Bundle;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private final String TAG = this.getClass().getName();
+	private RatingBar rBar;
+	private Socket socket;
+
+	private static final int SERVERPORT = 6000;
+
+	private static final String SERVER_IP = "10.0.2.2";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		
+
+		Button logtoken = (Button) findViewById(R.id.button1);
+
+		logtoken.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				runScanner();
+
+			}
+		});
+
+		updateLoyalityPoint();
+
+	}
+
+	private void updateLoyalityPoint() {
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		int loyalityPoints = sharedPrefs.getInt("points", 0);
+		rBar = (RatingBar) findViewById(R.id.ratingBar1);
+		rBar.setNumStars(loyalityPoints);
+
+	}
+
+	private void saveLoyalityPoints(int value) {
+
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Editor editor = sharedPrefs.edit();
+		sharedPrefs.edit().putInt("points", value);
+		editor.commit();
+
 	}
 
 	@Override
@@ -21,4 +78,112 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onResume() {
+
+		super.onResume();
+
+		updateLoyalityPoint();
+
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	private void runScanner() {
+
+		IntentIntegrator.initiateScan(this);
+
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(
+				requestCode, resultCode, intent);
+		if (scanResult.getContents() != null) {
+
+			String contents = intent.getStringExtra("SCAN_RESULT");
+			String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+			if (contents.matches("123456789")) {
+
+				Log.d(TAG, "scanner result= " + contents);
+				int points = (int) (rBar.getRating() + 1);
+				rBar.setRating(points);
+				saveLoyalityPoints(points);
+				
+				sendMessage("Blarg!");
+				if (points >= 5) {
+					Toast.makeText(this, "5 points you can claim free coffee",
+							Toast.LENGTH_LONG).show();
+				} else {
+
+				}
+
+			} else if (contents.matches("987654321")) {
+				rBar.setRating(0);
+				saveLoyalityPoints(0);
+			}
+
+		}
+	}
+	
+	private void sendMessage (String str){
+		PrintWriter out;
+		try {
+			out = new PrintWriter(new BufferedWriter(
+					
+					                    new OutputStreamWriter(socket.getOutputStream())),
+					
+					                    true);
+			 out.println(str);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+				           
+
+	}
+	class ClientThread implements Runnable {
+
+		 
+        @Override
+
+        public void run() {
+
+ 
+
+            try {
+
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+ 
+
+                socket = new Socket(serverAddr, SERVERPORT);
+
+ 
+
+            } catch (UnknownHostException e1) {
+
+                e1.printStackTrace();
+
+            } catch (IOException e1) {
+
+                e1.printStackTrace();
+
+            }
+
+ 
+
+        }
+
+	
+	}
+
 }
+
+
---
+++ CoffeeHack/src/com/qubecad/coffeehack/customerloyality/ServerMainActivity.java
@@ -0,0 +1,121 @@
+package com.qubecad.coffeehack.customerloyality;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ServerMainActivity extends Activity {
+	private ServerSocket serverSocket;
+
+	Handler updateConversationHandler;
+
+	Thread serverThread = null;
+
+	
+
+	public static final int SERVERPORT = 6000;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_server_main);
+		
+		updateConversationHandler = new Handler();
+		
+		 
+		
+		        this.serverThread = new Thread(new ServerThread());
+		
+		        this.serverThread.start();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.server_main, menu);
+		return true;
+	}
+	
+	class ServerThread implements Runnable {
+
+		public void run() {
+			Socket socket = null;
+			try {
+				serverSocket = new ServerSocket(SERVERPORT);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					socket = serverSocket.accept();
+
+					CommunicationThread commThread = new CommunicationThread(socket);
+					new Thread(commThread).start();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	class CommunicationThread implements Runnable {
+
+		private Socket clientSocket;
+
+		private BufferedReader input;
+
+		public CommunicationThread(Socket clientSocket) {
+
+			this.clientSocket = clientSocket;
+
+			try {
+
+				this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+
+			while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					String read = input.readLine();
+
+					updateConversationHandler.post(new updateUIThread(read));
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	class updateUIThread implements Runnable {
+		private String msg;
+
+		public updateUIThread(String str) {
+			this.msg = str;
+		}
+
+		@Override
+		public void run() {
+			//text.setText(text.getText().toString()+"Client Says: "+ msg + "\n");
+		}
+	}
+
+}
